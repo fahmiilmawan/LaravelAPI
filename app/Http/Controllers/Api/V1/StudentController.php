a607d26b4fb5840fb12db03ea75f77e3cd10479f
Fahmi Ilmawan <fahmikeilmawan22@gmail.com>
2025-04-09 21:43:43 +0700

    Done: Endpoint Search By NAMA, NIM and YMD

--- app/Http/Controllers/Api/V1/StudentController.php
+++ app/Http/Controllers/Api/V1/StudentController.php
@@ -3,10 +3,75 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\Clock\Clock;
 
 class StudentController extends Controller
 {
-   public 
+    public function search(Request $request)
+    {
+        $query = $request->only(['name', 'nim', 'ymd']);
+        $response = Http::get('https://bit.ly/48ejMhW');
+        $data = $response->body();
+        $parseData = json_decode($data);
+        $lines = explode("\n", trim($parseData->DATA));
+
+        $students = [];
+
+        foreach ($lines as $index => $line) {
+            if (empty(trim($line))) continue;
+
+            $parts = explode("|", $line);
+            if (count($parts) !== 3) continue;
+
+            //mapping field name
+            if ($index === 0) {
+                $fieldName= '';
+                $fieldNIM = '';
+                $fieldymd = '';
+                foreach ($parts as $key => $value) {
+                    // clock($value, $key);
+                    if ($value == 'NAMA') {
+                        $fieldName = $key;
+                    } elseif ($value == 'NIM') {
+                        $fieldNIM = $key;
+                    } elseif ($value == 'YMD') {
+                        $fieldymd = $key;
+                    }
+
+                }
+
+            }
+
+            clock($fieldName, $fieldNIM, $fieldymd);
+            if ($index !== 0) {
+                $student = [
+                    'name' => trim($parts[$fieldName]),
+                    'ymd' => trim($parts[$fieldymd]),
+                    'nim' => trim($parts[$fieldNIM])
+                ];
+                $students[] = $student;
+            }
+
+        }
+
+        $filtered = collect($students)->filter(function ($student) use ($query) {
+
+            foreach ($query as $key => $value) {
+                // if (strtolower($student[$key]) !== strtolower($value)) {
+                //     return false;
+                // }
+                if (!str_contains(strtolower($student[$key]) ,strtolower($value))) {
+                    return false;
+                }
+
+            }
+            return true;
+        });
+
+        return $filtered->values()->all();
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,10 +8,16 @@ Route::post('auth',[App\Http\Controllers\Api\V1\AuthController::class, 'login'])
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    //Login Auth
         Route::get('/index',[App\Http\Controllers\Api\V1\LoginController::class, 'index'])->name('index');
+    //CRUD Login
         Route::get('/show/{id}',[App\Http\Controllers\Api\V1\LoginController::class, 'show'])->name('show');
         Route::post('/store',[App\Http\Controllers\Api\V1\LoginController::class, 'store'])->name('store');
         Route::put('/update/{id}',[App\Http\Controllers\Api\V1\LoginController::class, 'update'])->name('update');
         Route::delete('/destroy/{id}',[App\Http\Controllers\Api\V1\LoginController::class, 'destroy'])->name('destroy');
+    //Search by Name
+        Route::get('/search',[App\Http\Controllers\Api\V1\StudentController::class, 'search'])->name('search');
     });
+
+
 
